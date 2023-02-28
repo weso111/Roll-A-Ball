141fcca2b927e267d97f46b13dafb9b56ba67d2d
weso111 <999tippy@gmail.com>
2023-02-28 12:01:24 +1100

    week 3 UI 28 FEB

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     int pickupCount;
+    int totalPickups;
     Timer timer;
     private Rigidbody rb;
+    bool wonGame = false;
+
+    [Header("UI")]
+    public GameObject winPanel;
+    public TMP_Text winTime;
+    public GameObject inGamePanel;
+    public TMP_Text timerText;
+    public TMP_Text pickupText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         //Get the number of pickups in our scene
-        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        totalPickups = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        pickupCount = totalPickups;
         //Display the pickup count
         CheckPickups();
         timer = FindObjectOfType<Timer>();
+        timer.StartTimer();
+
+        //Turn off our win panel
+        winPanel.SetActive(false);
+        inGamePanel.SetActive(true);
     }
     void FixedUpdate()
     {
@@ -26,8 +42,10 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+        //Update the timer text to that of the timer
+        timerText.text = "Time: " + timer.GetTime().ToString("F3");
 
-        
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -37,7 +55,7 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(other.gameObject);
             //Decrement the pickup count
-            pickupCount = -1;
+            pickupCount -= 1;
             CheckPickups();
             // Get the timer object
         }
@@ -45,8 +63,8 @@ public class PlayerController : MonoBehaviour
 
     void CheckPickups()
     {
-        Debug.Log("Pickups left:" + pickupCount);
-
+        //Debug.Log("Pickups left:" + pickupCount);
+        pickupText.text = "Pickups left:" + pickupCount;
 
         if (pickupCount == 0)
         {
@@ -56,10 +74,36 @@ public class PlayerController : MonoBehaviour
 
     void WinGame()
     {
+        if (wonGame == true)
+            return;
+
+
         timer.StopTimer();
-        Debug.Log("You Win!!! Your time was: " + timer.GetTime().ToString("F3"));
+        //Debug.Log("You Win!!! Your time was: " + timer.GetTime().ToString("F3"));
+        // inGamePanel on the next
+
+        inGamePanel.SetActive(false);
+        //Set the timer  on the text
+        winTime.text = "Your time was: " + timer.GetTime().ToString("F3");
+        //Turn on our win panel
+        winPanel.SetActive(true);
+
     }
 
+
+    //Temporary-Remove wjen doing modules in A2
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene
+            (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+
+    }
+
+        
    
+
+
+
+
 
 }
